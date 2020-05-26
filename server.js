c65e546d5347388b3ba3fbb8be255a2f849dadb6
JWeez <jwellstx@gmail.com>
2020-05-26 15:22:34 -0500

    initial setup for web scraper app

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// var db = require("./models");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+//app.use(flash());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(PORT, function () {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+})
